d08673b9ed20500b880ff31070815221c3e7d3d6
Zhang Kang <kang.zhang@intel.com>
2022-07-29 15:57:19 +0800

    egress ready

--- cmd/tc.go
+++ cmd/tc.go
@@ -2,25 +2,44 @@ package cmd
 
 import (
 	"fmt"
+	"golang.org/x/sys/unix"
 	"kangclzjc/nstc/pkg/ns"
 
 	"github.com/spf13/cobra"
+	"github.com/vishvananda/netlink"
 )
 
 func init() {
 	tcCommand.Flags().StringVarP(&namespace, "namespace", "n", "root", "give a network namespace")
 	tcCommand.Flags().StringVarP(&eth0, "eth0", "I", "eth0", "give a network interface in namespace")
-	tcCommand.Flags().Int64VarP(&egress, "egress", "e", 40000000, "give a egress bandwidth")
-	tcCommand.Flags().Int64VarP(&ingress, "ingress", "i", 40000000, "give a ingress bandwidth")
+	tcCommand.Flags().Uint64VarP(&egress, "egress", "e", 40000000, "give a egress bandwidth")
+	tcCommand.Flags().Uint64VarP(&ingress, "ingress", "i", 40000000, "give a ingress bandwidth")
 	rootCmd.AddCommand(tcCommand)
 }
 
 var (
 	namespace string
 	eth0      string
-	egress    int64
-	ingress   int64
+	egress    uint64
+	ingress   uint64
 )
+
+func safeQdiscList(link netlink.Link) ([]netlink.Qdisc, error) {
+	qdiscs, err := netlink.QdiscList(link)
+	if err != nil {
+		return nil, err
+	}
+	result := []netlink.Qdisc{}
+	for _, qdisc := range qdiscs {
+		// filter out pfifo_fast qdiscs because
+		// older kernels don't return them
+		_, pfifo := qdisc.(*netlink.PfifoFast)
+		if !pfifo {
+			result = append(result, qdisc)
+		}
+	}
+	return result, nil
+}
 
 var tcCommand = &cobra.Command{
 	Use:   "tc",
@@ -33,6 +52,110 @@ var tcCommand = &cobra.Command{
 			fmt.Printf("failed to open netns %q: %v", namespace, err)
 			return
 		}
+
 		defer netns.Close()
+
+		_ = netns.Do(func(_ ns.NetNS) error {
+			l, err := netlink.LinkByName(eth0)
+			fmt.Println("-------------zk 09-----------\n")
+			if err != nil {
+				fmt.Println("-------------zk-----------%s\n", err)
+			}
+
+			if err != nil {
+				fmt.Println("-------------zk-----------%s\n", err)
+			}
+			qdiscs, err := safeQdiscList(l)
+			if err != nil {
+				fmt.Println("-------------zk list qdisc-----------%s\n", err)
+			}
+			var htb *netlink.Htb
+			var hasHtb = false
+			for _, qdisc := range qdiscs {
+				fmt.Println("qdisc is %s\n", qdisc)
+
+				h, isHTB := qdisc.(*netlink.Htb)
+				if isHTB {
+					htb = h
+					hasHtb = true
+					break
+				}
+			}
+			fmt.Println("-------------zk 1-----------\n")
+
+			if !hasHtb {
+				// qdisc
+				// tc qdisc add dev lo root handle 1:0 htb default 1
+				attrs := netlink.QdiscAttrs{
+					LinkIndex: l.Attrs().Index,
+					Handle:    netlink.MakeHandle(1, 0),
+					Parent:    netlink.HANDLE_ROOT,
+				}
+				htb = netlink.NewHtb(attrs)
+				err = netlink.QdiscAdd(htb)
+				if err != nil {
+					fmt.Println("QdiscAdd error: %s\n", err)
+				}
+			}
+
+			// htb parent class
+			// tc class add dev lo parent 1:0 classid 1:1 htb rate 125Mbps ceil 125Mbps prio 0
+			// preconfig
+			classattrs1 := netlink.ClassAttrs{
+				LinkIndex: l.Attrs().Index,
+				Parent:    netlink.MakeHandle(1, 0),
+				Handle:    netlink.MakeHandle(1, 1),
+			}
+			htbclassattrs1 := netlink.HtbClassAttrs{
+				Rate:    10000000000,
+				Cbuffer: 0,
+			}
+			class1 := netlink.NewHtbClass(classattrs1, htbclassattrs1)
+			if err := netlink.ClassAdd(class1); err != nil {
+				fmt.Println("Class add error: ", err)
+			}
+
+			// htb child class
+			// tc class add dev lo parent 1:0 classid 1:5 htb rate 125kbps ceil 250kbps prio 0
+			classattrs2 := netlink.ClassAttrs{
+				LinkIndex: l.Attrs().Index,
+				Parent:    netlink.MakeHandle(1, 0),
+				Handle:    netlink.MakeHandle(1, 5),
+				//Handle: *linuxNetworkIO.ClassID,
+			}
+			htbclassattrs2 := netlink.HtbClassAttrs{
+				Rate:    egress,
+				Cbuffer: uint32(egress) * 2,
+			}
+			class2 := netlink.NewHtbClass(classattrs2, htbclassattrs2)
+			if err := netlink.ClassAdd(class2); err != nil {
+				fmt.Println("Class add error", err)
+			}
+
+			// filter add
+			// tc filter add dev lo parent 1:0 prio 0 protocol all handle 5 fw flowid 1:5
+			filterattrs := netlink.FilterAttrs{
+				LinkIndex: l.Attrs().Index,
+				Parent:    netlink.MakeHandle(1, 0),
+				Handle:    netlink.MakeHandle(1, 5),
+				Priority:  49152,
+				Protocol:  unix.ETH_P_IP,
+			}
+
+			filter := &netlink.GenericFilter{
+				filterattrs,
+				"cgroup",
+			}
+
+			if err != nil {
+				fmt.Println("failed to create NewFw(). Reason:%s", err)
+			}
+
+			if err := netlink.FilterAdd(filter); err != nil {
+				fmt.Println("failed to add filter. Reason:%s", err)
+			}
+
+			return nil
+		})
 	},
 }
--- go.mod
+++ go.mod
@@ -3,16 +3,24 @@ module kangclzjc/nstc
 go 1.18
 
 require (
+	github.com/containernetworking/plugins v1.1.1
+	github.com/onsi/ginkgo v1.16.5
+	github.com/onsi/gomega v1.20.0
 	github.com/spf13/cobra v1.5.0
 	github.com/spf13/viper v1.12.0
+	github.com/vishvananda/netlink v1.1.1-0.20210330154013-f5de75959ad5
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a
 )
 
 require (
+	github.com/containernetworking/cni v1.0.1 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/nxadm/tail v1.4.8 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
@@ -20,9 +28,11 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.3.0 // indirect
-	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	github.com/vishvananda/netns v0.0.0-20210104183010-2eb08e3e575f // indirect
+	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
