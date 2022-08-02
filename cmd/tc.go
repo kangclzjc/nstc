a0ef66ae14e30fe7ec7eb52e53468b543aee1703
Zhang Kang <kang.zhang@intel.com>
2022-08-02 16:53:42 +0800

    ifb0 ready

--- cmd/tc.go
+++ cmd/tc.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"golang.org/x/sys/unix"
 	"kangclzjc/nstc/pkg/ns"
+	"syscall"
 
 	"github.com/spf13/cobra"
 	"github.com/vishvananda/netlink"
@@ -13,16 +14,26 @@ func init() {
 	tcCommand.Flags().StringVarP(&namespace, "namespace", "n", "root", "give a network namespace")
 	tcCommand.Flags().StringVarP(&eth0, "eth0", "I", "eth0", "give a network interface in namespace")
 	tcCommand.Flags().Uint64VarP(&egress, "egress", "e", 40000000, "give a egress bandwidth")
+	tcCommand.Flags().Uint64VarP(&egressBurst, "egressBurst", "b", 40000000, "give a egress bandwidth burst")
+	tcCommand.Flags().Uint64VarP(&egressRate, "egressRate", "r", 40000000, "give a egress bandwidth rate")
 	tcCommand.Flags().Uint64VarP(&ingress, "ingress", "i", 40000000, "give a ingress bandwidth")
+	tcCommand.Flags().Uint64VarP(&ingressBurst, "ingressBurst", "u", 40000000, "give a ingress bandwidth burst")
+	tcCommand.Flags().Uint64VarP(&ingressRate, "ingressRate", "t", 40000000, "give a ingress bandwidth rate")
 	rootCmd.AddCommand(tcCommand)
 }
 
 var (
-	namespace string
-	eth0      string
-	egress    uint64
-	ingress   uint64
+	namespace    string
+	eth0         string
+	egress       uint64
+	egressBurst  uint64
+	egressRate   uint64
+	ingress      uint64
+	ingressBurst uint64
+	ingressRate  uint64
 )
+
+const latencyInMillis = 25
 
 func safeQdiscList(link netlink.Link) ([]netlink.Qdisc, error) {
 	qdiscs, err := netlink.QdiscList(link)
@@ -39,6 +50,120 @@ func safeQdiscList(link netlink.Link) ([]netlink.Qdisc, error) {
 		}
 	}
 	return result, nil
+}
+
+func getMTU(deviceName string) (int, error) {
+	link, err := netlink.LinkByName(deviceName)
+	if err != nil {
+		return -1, err
+	}
+
+	return link.Attrs().MTU, nil
+}
+
+func time2Tick(time uint32) uint32 {
+	return uint32(float64(time) * float64(netlink.TickInUsec()))
+}
+
+func latencyInUsec(latencyInMillis float64) float64 {
+	return float64(netlink.TIME_UNITS_PER_SEC) * (latencyInMillis / 1000.0)
+}
+
+func buffer(rate uint64, burst uint32) uint32 {
+	return time2Tick(uint32(float64(burst) * float64(netlink.TIME_UNITS_PER_SEC) / float64(rate)))
+}
+
+func limit(rate uint64, latency float64, buffer uint32) uint32 {
+	return uint32(float64(rate)*latency/float64(netlink.TIME_UNITS_PER_SEC)) + buffer
+}
+
+func createTBF(rateInBits, burstInBits uint64, linkIndex int) error {
+	// Equivalent to
+	// tc qdisc add dev link root tbf
+	//		rate netConf.BandwidthLimits.Rate
+	//		burst netConf.BandwidthLimits.Burst
+	if rateInBits <= 0 {
+		return fmt.Errorf("invalid rate: %d", rateInBits)
+	}
+	if burstInBits <= 0 {
+		return fmt.Errorf("invalid burst: %d", burstInBits)
+	}
+	rateInBytes := rateInBits / 8
+	burstInBytes := burstInBits / 8
+	bufferInBytes := buffer(uint64(rateInBytes), uint32(burstInBytes))
+	latency := latencyInUsec(latencyInMillis)
+	limitInBytes := limit(uint64(rateInBytes), latency, uint32(burstInBytes))
+
+	qdisc := &netlink.Tbf{
+		QdiscAttrs: netlink.QdiscAttrs{
+			LinkIndex: linkIndex,
+			Handle:    netlink.MakeHandle(1, 0),
+			Parent:    netlink.HANDLE_ROOT,
+		},
+		Limit:  uint32(limitInBytes),
+		Rate:   uint64(rateInBytes),
+		Buffer: uint32(bufferInBytes),
+	}
+	err := netlink.QdiscAdd(qdisc)
+	if err != nil {
+		return fmt.Errorf("create qdisc: %s", err)
+	}
+	return nil
+}
+
+func CreateEgressQdisc(rateInBits, burstInBits uint64, hostDeviceName string, ifbDeviceName string) error {
+	ifbDevice, err := netlink.LinkByName(ifbDeviceName)
+	if err != nil {
+		return fmt.Errorf("get ifb device: %s", err)
+	}
+	hostDevice, err := netlink.LinkByName(hostDeviceName)
+	if err != nil {
+		return fmt.Errorf("get host device: %s", err)
+	}
+
+	// add qdisc ingress on host device
+	ingress := &netlink.Ingress{
+		QdiscAttrs: netlink.QdiscAttrs{
+			LinkIndex: hostDevice.Attrs().Index,
+			Handle:    netlink.MakeHandle(0xffff, 0), // ffff:
+			Parent:    netlink.HANDLE_INGRESS,
+		},
+	}
+
+	err = netlink.QdiscAdd(ingress)
+	if err != nil {
+		fmt.Errorf("create ingress qdisc: %s\n", err)
+	}
+
+	// add filter on host device to mirror traffic to ifb device
+	filter := &netlink.U32{
+		FilterAttrs: netlink.FilterAttrs{
+			LinkIndex: hostDevice.Attrs().Index,
+			Parent:    ingress.QdiscAttrs.Handle,
+			Priority:  1,
+			Protocol:  syscall.ETH_P_ALL,
+		},
+		ClassId:    netlink.MakeHandle(1, 1),
+		RedirIndex: ifbDevice.Attrs().Index,
+		Actions: []netlink.Action{
+			&netlink.MirredAction{
+				ActionAttrs:  netlink.ActionAttrs{},
+				MirredAction: netlink.TCA_EGRESS_REDIR,
+				Ifindex:      ifbDevice.Attrs().Index,
+			},
+		},
+	}
+	err = netlink.FilterAdd(filter)
+	if err != nil {
+		fmt.Errorf("add filter: %s\n", err)
+	}
+
+	// throttle traffic on ifb device
+	err = createTBF(rateInBits, burstInBits, ifbDevice.Attrs().Index)
+	if err != nil {
+		return fmt.Errorf("create ifb qdisc: %s\n", err)
+	}
+	return nil
 }
 
 var tcCommand = &cobra.Command{
@@ -149,8 +274,24 @@ var tcCommand = &cobra.Command{
 			}
 
 			// ingress
-
+			// tc filter add dev ens3f3 parent ffff: protocol ip u32 match u32 0 0 action mirred egress redirect dev ifb0
 			// set egress for ifb
+			mtu, err := getMTU(eth0)
+			if err != nil {
+				fmt.Println("failed to get MTU. Reason:%s", err)
+			}
+
+			ifbDeviceName := "ifb0"
+			err = ns.CreateIfb(ifbDeviceName, mtu)
+			if err != nil {
+				fmt.Println("failed to create ifb0. Reason:%s", err)
+			}
+
+			fmt.Println("create ifb success")
+			err = CreateEgressQdisc(egress, egressBurst, eth0, ifbDeviceName)
+			if err != nil {
+				fmt.Println("failed to create egress qdisc. Reason:%s", err)
+			}
 
 			return nil
 		})
